640a93a5a1d2e7284f6d7f24d022f89d6172f3b4
Fish-win <2019kkvb@gmail.com>
2022-01-30 20:53:20 +0800

    hot100-字符串-暴力剪枝

---
+++ Leetcode/5.最长回文子串.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=cpp
+ *
+ * [5] 最长回文子串
+ */
+
+// @lc code=start
+#include <string>
+#include <algorithm>
+using namespace std;
+
+// 暴力 + 剪枝
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        str = s;
+        int maxl, maxr;
+        for (int l = 0; l < s.size(); ++l) {
+            for (int r = l; r < s.size(); ++r) {
+                if ((r-l > maxr-maxl) && isPalindrome(l, r)) {
+                    maxr = r;
+                    maxl = l;
+                }
+            }
+        }
+        return s.substr(maxl, maxr-maxl+1);
+    }
+private:
+    string str;
+    bool isPalindrome(int l, int r) {
+        while (str[l] == str[r]) {
+            if (l++ >= r--) return true;
+        }
+        return false;
+    }
+};
+// @lc code=end
+
